7a311f9941e4bebbc9b8f9a847cc7a06bd924187
hannaharubin <xne7fa@virginia.edu>
2025-06-10 12:06:57 -0400

    changes

--- hannah.py/project.py
+++ hannah.py/project.py
@@ -2,8 +2,13 @@
 import random
 import uvage
 image_number=0
+walker_image=uvage.load_sprite_sheet("run.png")
+fly_image=uvage.load_sprite_sheet("fly.png")
 background=uvage.display.set_mode((800,600))
 def tick():
     if uvage.is_pressing("up"):
         image_number+=1
-    player_icon.image= #insert sprite
+    player_icon.image=fly_image
+    if uvage.is_pressing("down"):
+        image_number+=1
+    player_icon.image=fly_image
