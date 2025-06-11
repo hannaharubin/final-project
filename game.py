300f0f7398cc141b79a0089287ddf314e792b706
prithika <prithikakundur@gmail.com>
2025-06-11 13:09:46 -0400

    version

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ score = 0
 # Images
 run_image = "run.png"
 fly_image = "fly.png"
-ground1 = "ground.png"
-ground2 = "ground copy.png"
+ground = "ground.png"
+
 
 # Player
 player_icon = uvage.from_image(150, 400, run_image)
@@ -21,8 +21,9 @@ max_jumps = 3  #  double jump
 
 # Obstacle
 ground_speed = 5
-ground1 = uvage.from_image(0, 450, ground1)
-ground2 = uvage.from_image(900, 450, ground2)  
+ground.x = 0
+border1 = 0
+border2 = 0
 
 
 def tick():
@@ -31,18 +32,26 @@ def tick():
     global ground
     global ground_speed
     global ground1, ground2
+    global border1
+    global border2
     camera.clear("white")
 
 
-    if not game_over:
+    while not game_over:
         player_icon.yspeed += 1 # The += 1 represents gravity adding downwards speed every tick; positive y means downward direction
         if player_icon.y == 400:
             player_icon.yspeed = 0 # This stops us from falling through the floor
             if uvage.is_pressing("up arrow"): # Check if we should jump
                 player_icon.yspeed = - 15
         player_icon.move_speed() # THIS IS REALLY IMPORTANT, or the walker won't move based on its speed
-        ground1.x-=ground_speed
-        ground2.x=ground_speed
+        ground.x-=1
+        border1.x-=1
+        if border1.x <= -800:
+            border1.x = 800
+        if ground.x <= -800:
+            ground.x = 800
+
+
         
     
             
