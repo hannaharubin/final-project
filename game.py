a9a3281f41bd38098e7953285fa33a1e4b3c0ae1
prithika <prithikakundur@gmail.com>
2025-06-11 15:36:33 -0400

    game so far

--- game.py
+++ game.py
@@ -1,77 +1,78 @@
 import uvage
 import random
 
-# Setup
-camera = uvage.Camera(800, 600)
+camera = uvage.Camera(800, 550)
 game_over = False
 score = 0
 
 # Images
 run_image = "run.png"
 fly_image = "fly.png"
-ground = "ground.png"
-
+ground_image = "desert4.jpg"
 
 # Player
-player_icon = uvage.from_image(150, 400, run_image)
-player_vy = 0
+player_icon = uvage.from_image(150, 375, run_image)
 gravity = 1
 jumping = False
-max_jumps = 3  #  double jump
 
-# Obstacle
-ground_speed = 5
-ground.x = 0
-border1 = 0
-border2 = 0
+# Obstacles
+obstacles = []
+frame_count = 0
 
+# Ground
+scroll_speed = 5
+scroll_x = 0
+bg1 = uvage.from_image(400, 275, "desert4.jpg")
+bg1.scale_by(.431)
+bg2 = uvage.from_image(1200, 275, "desert4.jpg")
+bg2.scale_by(.431)
+
+# ground = uvage.from_image(400, 300, ground)  
+# ground.scale_by(1)  # reduce size if needed
+
+ground_width = bg1.width
 
 def tick():
-    global game_over, score, player_vy, jumping
-    global player_icon
-    global ground
-    global ground_speed
-    global ground1, ground2
-    global border1
-    global border2
+    global game_over, score
+    global jumping, player_icon, gravity
+    global scroll_x, scroll_speed
+    global bg1, bg2
+    global frame_count, obstacles
+
     camera.clear("white")
 
+    scroll_x = (scroll_x + scroll_speed) % ground_width
 
-    while not game_over:
-        player_icon.yspeed += 1 # The += 1 represents gravity adding downwards speed every tick; positive y means downward direction
-        if player_icon.y == 400:
+    bg1.x = ground_width//2 - scroll_x
+    bg2.x = (3*ground_width)//2 - scroll_x
+
+
+    if not game_over:
+        player_icon.yspeed += gravity # The += 1 represents gravity adding downwards speed every tick; positive y means downward direction
+        frame_count+=1
+      #  if frame_count % random.randint(60, 120) == 0:
+      #      new_obstacle = uvage.from_image(850, 400,"" )
+        if player_icon.y == 375:
             player_icon.yspeed = 0 # This stops us from falling through the floor
             if uvage.is_pressing("up arrow"): # Check if we should jump
                 player_icon.yspeed = - 15
         player_icon.move_speed() # THIS IS REALLY IMPORTANT, or the walker won't move based on its speed
-        ground.x-=1
-        border1.x-=1
-        if border1.x <= -800:
-            border1.x = 800
-        if ground.x <= -800:
-            ground.x = 800
-
-
-        
     
-            
-            
-
-        # Move obstacle
+      # Move obstacle
       #  obstacle.x -= obstacle_speed
       #  if obstacle.x < -50:
       #      obstacle.x = 850
       #      score += 1
 
-        # player_icon".touches(obstacle)" is important & the only way i could find to check for collision
-     #   if player_icon.touches(obstacle):
+      # player_icon".touches(obstacle)" is important & the only way i could find to check for collision
+      #   if player_icon.touches(obstacle):
       #      game_over = True
 
         # Draw game elements
+        camera.draw(bg1)
+        camera.draw(bg2)
         camera.draw(player_icon)
-        camera.draw(ground1)
-        camera.draw(ground2)
-        camera.draw(uvage.from_text(80, 50, f"Score: {score}", 30, "black"))
+        camera.draw(uvage.from_text(80, 50, "Score: {score}", 30, "black"))
     else:
         camera.draw(uvage.from_text(400, 300, "Game Over", 60, "red"))
 
